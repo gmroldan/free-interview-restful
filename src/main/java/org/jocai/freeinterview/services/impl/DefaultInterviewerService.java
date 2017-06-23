7a3182bbd411ef83c64b20faf9180f2a04bbfbeb
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2017-06-23 12:39:13 -0300

    #1 - Configure logging framework

--- src/main/java/org/jocai/freeinterview/services/impl/DefaultInterviewerService.java
+++ src/main/java/org/jocai/freeinterview/services/impl/DefaultInterviewerService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.jocai.freeinterview.model.Interviewer;
 import org.jocai.freeinterview.repository.InterviewerRepository;
 import org.jocai.freeinterview.services.InterviewerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -13,12 +15,17 @@ import org.springframework.util.Assert;
  */
 @Service
 public class DefaultInterviewerService implements InterviewerService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultInterviewerService.class);
+
     @Autowired
     private InterviewerRepository interviewerRepository;
 
     @Override
     public Interviewer getInteviewer(final Long id) {
         Assert.notNull(id, "The id cannot be null.");
+
+        LOGGER.info("Searching interviewer with id " + id);
+
         return this.interviewerRepository.findOne(id);
     }
 
